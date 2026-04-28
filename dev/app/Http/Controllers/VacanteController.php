fd0a5a48a13de089dea4d44f9f626095de7cca2c
JaimeValdezz <jaime.quinonez.itid@unipolidgo.edu.mx>
2026-04-28 15:02:52 -0600

    Update VacanteController.php

--- dev/app/Http/Controllers/VacanteController.php
+++ dev/app/Http/Controllers/VacanteController.php
@@ -1,3 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Google\Cloud\Firestore\FirestoreClient;
 
 class VacanteController extends Controller
